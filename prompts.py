d05fa44352c63b35779525d07f1bf587d7b6da2b
Igor Popov <gonadarian@gmail.com>
2025-07-15 15:47:34 +0200

    refactor: move prompts & words to separate files

---
+++ prompts.py
@@ -0,0 +1,24 @@
+all_prompts = {
+    "kawaii":
+        "A {term} in kawaii style using pastel colors.",
+    "kawaii_simple":
+        "A {term}. Simple drawing, few pastel colors. In kawaii style.",
+    "kawaii_lines":
+        "A {term} in kawaii style. Simple drawing, but enough details for drawing to be recognizable."
+        "Use few pastel colors, clean fill, no gradients. Use lines with clean edges",
+    "kawaii_small_eyes":
+        "Create an image of a {term}, in Kawaii style. Don't draw anything around the image. "
+        "Don't draw too big eyes. Use pastel colors.",
+    "kawaii_no_eyes":
+        "Create an image of a {term}, in Kawaii style, but without the eyes. "
+        "Don't draw anything around the image. Use pastel colors.",
+    "duolingo":
+        "A {term}. Simple drawing, few pastel colors. In duolingo style.",
+    "cute":
+        "Create an image of a {term}, illustrated in a simple and cute manner, using few pastel colors. "
+        "Incorporate a common style often seen in cute and endearing styles of animation, characterized "
+        "by large, expressive eyes and rounded shapes. The colors used should be calming, soft, and muted, "
+        "reminiscent of the hues commonly found in pastel art pieces.",
+    "rebus":
+        "Create a simple image, as a drawing with few pastel colors. Draw a {term} with white background.",
+}
---
+++ words.py
@@ -0,0 +1,127 @@
+words_pronounce = [
+    "bow|baʊ",
+    "bow|boʊ",
+    "bow",
+]
+
+words_flupe_7 = [
+    "fig",
+    "kiwi cut in half, viewed from side",
+    "melon",
+    "blackberry",
+    "mango",
+    "groceries",
+    "shoe",
+    "clothes",
+    "toy",
+    "book",
+    "shop",
+    "bear",
+    "parade",
+    "present",
+    "binoculars",
+    "princess",
+    "prince"
+]
+
+words_flupe_8 = [
+    "butterfly",
+    "world",
+    "lake",
+    "river",
+    "stream",
+    "leaf",
+    "toe",
+    "float",
+    "motor boat",
+    "sailing boat",
+    "rowing boat",
+    "paddle boat",
+    "canoe",
+]
+
+words_flupe_9 = [
+    "robin",
+    "wagtail",
+    "seagull",
+    "pigeon",
+    "wing",
+    "fly",
+    "jump",
+    "march",
+    "giant",
+    "tiny",
+    "forward",
+    "backward",
+    "tomato",
+    "broccoli",
+    "cucumber",
+    "peas",
+    "spinach",
+]
+
+words_flupe_10 = [
+    "sun",
+    "rain",
+    "snow",
+    "storm",
+    "oars",
+    "umbrella",
+    "scarf",
+    "boots",
+    "forest",
+    "gloves",
+    "earmuffs",
+    "train",
+]
+
+words_flupe_11 = [
+    "traffic lights",
+    "zebra crossing",
+    "bus stop",
+    "stop sign",
+    "seatbelt",
+    "bed",
+    "sleep",
+    "brick",
+    "barn",
+    "car",
+    "owl",
+    "beak",
+    "claws",
+    "road",
+    "bus",
+]
+
+words_flupe_12 = [
+    "spring",
+    "summer",
+    "autumn",
+    "winter",
+    "lantern",
+    "star",
+    "twinkle",
+    "cloud",
+    "flower",
+    "cave",
+    "snore",
+    "dark",
+    "light",
+    "t-shirt",
+    "socks",
+    "jumper",
+    "pyjamas",
+    "nightdress",
+    "trousers",
+]
+
+all_words = {
+    "flupe": {
+        7: words_flupe_7,
+        8: words_flupe_8,
+        9: words_flupe_9,
+        10: words_flupe_10,
+        11: words_flupe_11,
+        12: words_flupe_12
+    }
+}
